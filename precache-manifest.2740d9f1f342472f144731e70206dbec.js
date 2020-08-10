223acaee6c9f386b58dfb8bf52f00ce206095df3
Roman Kuksin <rkuksin.cpp@gmail.com>
2020-08-10 21:55:22 +0700

    Updates

---
+++ precache-manifest.2740d9f1f342472f144731e70206dbec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bcc46b8dde7f4750067fff204579b4",
+    "url": "/self-help-sheets/index.html"
+  },
+  {
+    "revision": "1f9d6a2f834eae2a78da",
+    "url": "/self-help-sheets/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b8c38049b6e681f4009",
+    "url": "/self-help-sheets/static/js/2.541b8925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/self-help-sheets/static/js/2.541b8925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9d6a2f834eae2a78da",
+    "url": "/self-help-sheets/static/js/main.29d3e035.chunk.js"
+  },
+  {
+    "revision": "af6530dd8458c63c8042",
+    "url": "/self-help-sheets/static/js/runtime-main.85fa248b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/self-help-sheets/static/media/logo.5d5d9eef.svg"
+  }
+]);
