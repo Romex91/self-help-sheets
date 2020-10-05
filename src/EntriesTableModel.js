0477916ac98adcdfd606a8fbcaecb24f9fb08236
Roman Kuksin <rkuksin.cpp@gmail.com>
2020-10-05 15:59:48 +0700

    quit using private methods to avoid crippled stack traces

--- src/EntriesTableModel.js
+++ src/EntriesTableModel.js
@@ -20,40 +20,73 @@ export class EntriesTableModel extends Interface {
 }
 
 export class EntriesTableModelImpl extends EntriesTableModel {
-  #backendMap = null;
-  #keys = [];
-  #entries = null;
-  #subscriptions = new Set();
-
   constructor(backendMap, authClient) {
     console.assert(backendMap instanceof BackendMap);
     super();
-    this.#backendMap = backendMap;
-    this.#init(authClient);
+    this._backendMap = backendMap;
+    this._init(authClient);
   }
 
-  #init = async (authClient) => {
+  subscribe(callback) {
+    this._subscriptions.add(callback);
+    if (!!this._entries && this._entries.length > 0)
+      callback(this._getFilteredEntries());
+  }
+
+  unsubscribe(callback) {
+    this._subscriptions.delete(callback);
+  }
+
+  undo() {}
+
+  redo() {}
+
+  sync() {}
+
+  onUpdate = (entry) => {
+    let prevEntry = this._entries.find((x) => x.key === entry.key);
+    if (prevEntry == null) return;
+
+    this._entries = this._entries.map((x) => (x.key === entry.key ? entry : x));
+    if (
+      entry.data === EntryStatus.LOADING &&
+      prevEntry.data === EntryStatus.HIDDEN
+    ) {
+      this._fetch(entry.key);
+    } else if (entry.key !== null && entry.isDataLoaded()) {
+      this._backendMap.set(entry.key, JSON.stringify(entry.data));
+    }
+
+    this._onEntriesChanged();
+  };
+
+  _backendMap = null;
+  _keys = [];
+  _entries = null;
+  _subscriptions = new Set();
+
+  _init = async (authClient) => {
     while (authClient.state !== GDriveStates.SIGNED_IN) {
       await authClient.waitForStateChange();
     }
-    this.#keys = (await this.#backendMap.getAllKeys()).reverse();
+    this._keys = (await this._backendMap.getAllKeys()).reverse();
 
     let entryNumber = 0;
-    this.#entries = this.#keys.map((key) => {
+    this._entries = this._keys.map((key) => {
       if (entryNumber++ < 30) {
-        this.#fetch(key.id);
+        this._fetch(key.id);
         return new EntryModel(key.id, EntryStatus.LOADING);
       } else {
         return new EntryModel(key.id, EntryStatus.HIDDEN);
       }
     });
 
-    this.#onEntriesChanged();
+    this._onEntriesChanged();
   };
 
-  #fetch = async (key) => {
-    let content = await this.#backendMap.get(key);
-    let entryIndex = this.#entries.findIndex((x) => x.key === key);
+  _fetch = async (key) => {
+    let content = await this._backendMap.get(key);
+    let entryIndex = this._entries.findIndex((x) => x.key === key);
     if (entryIndex === -1) {
       console.error("Entry for fetch doesn't exist anymore. " + key);
       return;
@@ -61,14 +94,14 @@ export class EntriesTableModelImpl extends EntriesTableModel {
 
     if (content === undefined) {
       console.error("Key " + key + " is missing");
-      this.#entries.splice(entryIndex, 1);
-      this.#onEntriesChanged();
+      this._entries.splice(entryIndex, 1);
+      this._onEntriesChanged();
       return;
     }
 
     try {
       if (content === "") {
-        this.#entries[entryIndex] = new EntryModel(key, {}).delete();
+        this._entries[entryIndex] = new EntryModel(key, {}).delete();
       } else {
         const data = JSON.parse(content);
 
@@ -79,22 +112,22 @@ export class EntriesTableModelImpl extends EntriesTableModel {
           throw new Error("bad format " + content);
         }
 
-        this.#entries[entryIndex] = new EntryModel(key, data);
+        this._entries[entryIndex] = new EntryModel(key, data);
       }
     } catch (e) {
       console.error(e.message + " " + content);
-      this.#entries.splice(entryIndex, 1);
-      this.#backendMap.delete(key);
+      this._entries.splice(entryIndex, 1);
+      this._backendMap.delete(key);
     }
 
-    this.#onEntriesChanged();
+    this._onEntriesChanged();
   };
 
   // If user deletes entries from top of the table than keys assigned to these
   // entries can be reused again. This way syncing is a bit simpler.
-  #getLastFreeIndex = () => {
+  _getLastFreeIndex = () => {
     let lastFreeIndex = -1;
-    for (let entry of this.#entries) {
+    for (let entry of this._entries) {
       if (
         entry.isDataLoaded() &&
         (entry.data === EntryStatus.DELETED ||
@@ -108,46 +141,46 @@ export class EntriesTableModelImpl extends EntriesTableModel {
     return lastFreeIndex;
   };
 
-  #onEntriesChanged = async () => {
+  _onEntriesChanged = async () => {
     // There always should be an empty entry on top of the table.
     // The table doesn't have ADD NEW ITEM button. Instead user should
     // start typing in the top empty entry to add a new one.
-    this.#notifySubscribers();
+    this._notifySubscribers();
     if (
-      this.#entries.length === 0 ||
-      (this.#getLastFreeIndex() === -1 && this.#entries[0].isDataLoaded())
+      this._entries.length === 0 ||
+      (this._getLastFreeIndex() === -1 && this._entries[0].isDataLoaded())
     ) {
-      if (this.#entries.length > 0 && this.#entries[0].key === null) {
+      if (this._entries.length > 0 && this._entries[0].key === null) {
         return;
       }
 
-      this.#entries.unshift(new EntryModel(null, {}).clear());
+      this._entries.unshift(new EntryModel(null, {}).clear());
 
-      let newKey = await this.#backendMap.createKey();
+      let newKey = await this._backendMap.createKey();
 
-      if (this.#entries[0].key !== null) {
+      if (this._entries[0].key !== null) {
         throw new Error(
           "A null key should be ontop until the new key is created"
         );
       }
 
-      this.#entries[0] = new EntryModel(newKey, this.#entries[0].data);
+      this._entries[0] = new EntryModel(newKey, this._entries[0].data);
 
-      let md5Checksum = await this.#backendMap.set(
+      let md5Checksum = await this._backendMap.set(
         newKey,
-        JSON.stringify(this.#entries[0].data)
+        JSON.stringify(this._entries[0].data)
       );
 
-      this.#keys.unshift({ md5Checksum, id: newKey });
+      this._keys.unshift({ md5Checksum, id: newKey });
 
-      await this.#onEntriesChanged();
+      await this._onEntriesChanged();
       return;
     }
   };
 
-  #getFilteredEntries = () => {
-    let lastFreeIndex = this.#getLastFreeIndex();
-    let filteredEntries = this.#entries.slice(
+  _getFilteredEntries = () => {
+    let lastFreeIndex = this._getLastFreeIndex();
+    let filteredEntries = this._entries.slice(
       lastFreeIndex > 0 ? lastFreeIndex : 0
     );
 
@@ -161,43 +194,10 @@ export class EntriesTableModelImpl extends EntriesTableModel {
     return filteredEntries;
   };
 
-  #notifySubscribers = () => {
-    if (this.#entries.length === 0) return;
-    this.#subscriptions.forEach((callback) => {
-      callback(this.#getFilteredEntries());
+  _notifySubscribers = () => {
+    if (this._entries.length === 0) return;
+    this._subscriptions.forEach((callback) => {
+      callback(this._getFilteredEntries());
     });
-  };
-
-  subscribe(callback) {
-    this.#subscriptions.add(callback);
-    if (!!this.#entries && this.#entries.length > 0)
-      callback(this.#getFilteredEntries());
-  }
-
-  unsubscribe(callback) {
-    this.#subscriptions.delete(callback);
-  }
-
-  undo() {}
-
-  redo() {}
-
-  sync() {}
-
-  onUpdate = (entry) => {
-    let prevEntry = this.#entries.find((x) => x.key === entry.key);
-    if (prevEntry == null) return;
-
-    this.#entries = this.#entries.map((x) => (x.key === entry.key ? entry : x));
-    if (
-      entry.data === EntryStatus.LOADING &&
-      prevEntry.data === EntryStatus.HIDDEN
-    ) {
-      this.#fetch(entry.key);
-    } else if (entry.key !== null && entry.isDataLoaded()) {
-      this.#backendMap.set(entry.key, JSON.stringify(entry.data));
-    }
-
-    this.#onEntriesChanged();
   };
 }
